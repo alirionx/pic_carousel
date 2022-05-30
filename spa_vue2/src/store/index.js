0f1a891605c5fd4e50f8d36b50a68b24c56776d0
alirionx <dquilitzsch@outlook.de>
2022-05-31 01:33:39 +0200

    vuex richdig!

--- spa_vue2/src/store/index.js
+++ spa_vue2/src/store/index.js
@@ -9,21 +9,42 @@ export default new Vuex.Store({
   state: {
     username: null,
     role: null,
+    bearer: null
   },
   getters: {
   },
   mutations: {
+    set_userdata(state, payload){
+      state.username = payload.username;
+      state.role = payload.role;
+      state.bearer = payload.bearer;
+    },
+    reset_userdata(state){
+      state.username = null;
+      state.role = null;
+      state.bearer = null;
+    },
+
   },
   actions: {
     login(context, token){
       let tokenData = JSON.parse(atob(token.split('.')[1]));
       console.log(tokenData)
-      this.state.username = tokenData.username
-      this.state.role = tokenData.role
+      
+      let payload = {
+        bearer: token,
+        username: tokenData.username,
+        role: tokenData.role
+      }
+      context.commit('set_userdata', payload) // => SAUBER!!!
+      // this.state.username = tokenData.username
+      // this.state.role = tokenData.role
     },
-    logout(){
-      this.state.username = null
-      this.state.role = null
+
+    logout(context){
+      context.commit('reset_userdata')
+      // this.state.username = null
+      // this.state.role = null
     }
   },
   modules: {
